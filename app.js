1ecac4bcc9441cec60cc53fe8a5e19dd30c1ac09
Saira Batool <sairabatool787@gmail.com>
2024-07-06 09:36:16 +0500

    Basics

---
+++ app.js
@@ -0,0 +1,18 @@
+/*Prefer not to use var because of issuse in block scope and functional scvope */
+console.log("Saira")
+
+// Variable
+const accountId = 123345;
+let accountEmail = "sairabatool787@gmail.com";
+var accountPassword = "Saira123gggg";
+let acountstate;
+
+// accountId = 3;
+accountPassword = 23
+accountEmail = "sajidabaool123@gmail.com";
+
+// console.log(accountId)
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,acountstate])
