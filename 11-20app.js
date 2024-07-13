37af794eca3c8401affbce145d0057ce106cf779
Saira Batool <sairabatool787@gmail.com>
2024-07-13 06:38:12 +0500

    Done

--- 11-20app.js
+++ 11-20app.js
@@ -70,3 +70,45 @@ var max = 20
 
                                            //range
 console.log(Math.floor(Math.random() * (max - min + 1)) + min); //12
+
+
+// Date and Time 
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toTimeString());
+// Sat Jul 13 2024 05:49:26 GMT+0500 (Pakistan Standard Time)
+// 2024-07-13T00:49:26.060Z
+// 2024-07-13T00:49:26.060Z
+// 7/13/2024
+// 7/13/2024, 5:49:26 AM
+// 05:49:26 GMT+0500 (Pakistan Standard Time)
+
+console.log(typeof(myDate)); //object
+
+let myCreateDate = new Date(2023, 0, 23) //single digit mai counting like array js take a month from 0 like jan 
+console.log(myCreateDate); //2023-01-22T19:00:00.000Z
+console.log(myCreateDate.toDateString()); //Mon Jan 23 2023
+
+let myCreatedDate1 = new Date("2023-01-23") //YY MM DD
+console.log(myCreatedDate1.toDateString()); //Mon Jan 23 2023
+
+let myCreatedDate2 = new Date("2023-01-23") //MM DD YY
+console.log(myCreatedDate2.toDateString()); //Mon Jan 23 2023
+
+// let myTimeStamp = new Date() //2024-07-13T01:04:05.004Z
+// OR 
+let myTimeStamp = Date.now() //1720832786313
+console.log(myTimeStamp) 
+console.log(myCreateDate.getTime()); //1674414000000
+console.log(Math.floor(Date.now()/1000)); //milisec convert in second it gives decimal value OrMath.Round()
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()); //6 //console.log(newDate.getMonth() + 1);
+
+console.log(newDate.getDay()); //6
