d07199f2e122bc268f3d59cd398dcc2e555d458d
SairaBatool123 <sairabatool787@gmail.com>
2024-10-26 23:21:48 +0500

    Working ON IT

---
+++ JS_Projects/app.js
@@ -0,0 +1,61 @@
+// let select = function(){
+//     let choose = this.attr('data-filter');
+   
+// }
+// `${'.gallery-items'}`
+
+
+
+// var select = $(this).attr('data-filter');
+// $('.gallery-items').isotope({
+//     filter:selector
+// });
+// return false;
+// });
+
+
+// Select the element with the class "nature"
+// let liNature = document.querySelector('.nature');
+// console.log(liNature);
+
+// liNature.addEventListener('click', (event) => {
+//   event.preventDefault();
+  
+//   // Select all elements with the class "nature"
+//   let lilist = document.querySelectorAll('.nature');
+
+//   // Remove the "fashion" class from liNature
+//   liNature.classList.remove('fashion'); // Remove without the dot
+
+//   // Update the innerHTML of liNature with some content or perform any action on each element
+//   let listContent = '';
+//   lilist.forEach((element) => {
+//     listContent += `<li>${element.innerHTML}</li>`; // Append each element's content
+//     console.log(element); // Log each element
+//   });
+  
+//   // Set innerHTML of liNature to show the list
+//   liNature.innerHTML = listContent;
+// });
+
+
+
+// var liNature = document.querySelector('.nature')
+// console.log(liNature);
+
+// liNature.addEventListener('click',(event)=>{
+//     event.preventDefault();
+//     let lilist = document.querySelectorAll('.nature')
+//     // liNature.classList.remove('.fashion')
+//     lilist.forEach(()=>{
+//         console.log(lilist);
+        
+//     })
+//     liNature.innerHTML = lilist
+    
+//     // liNature.classList.add('nature')
+//     // console.log(liNature);
+    
+// })
+// console.log(liNature);
+ 
